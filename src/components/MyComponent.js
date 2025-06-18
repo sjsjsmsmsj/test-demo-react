db03de31955b538e6eeea4cdb57f10cb485584f6
sjsjsmsmsj <lamgiathinh05@gmail.com>
2025-06-18 16:10:28 +0700

    add some sSetState

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -7,13 +7,16 @@ class MyComponent extends React.Component {
         age: 20
     }; 
     
-    handleClick(event) {
-        console.log(event.target)
-        console.log("My name is ", this.state.name)
+    handleClick = (event) => {
+        // console.log("Random: ", Math.floor(Math.random() * 100 + 1))
+        this.setState({
+            name: "Lam gia thinh",
+            age: Math.floor(Math.random() * 100 + 1)
+        })
     }
 
     handleOnMoreOver(event) {
-        console.log(event.pageX)
+        // console.log(event.pageX)
     }
 
     // JSX
@@ -22,7 +25,7 @@ class MyComponent extends React.Component {
             <div>
                 My name is {this.state.name} and I'm from {this.state.address}
                 <button onMouseOver={this.handleOnMoreOver}>Hover me</button>
-                <button onClick={this.handleClick}>Click me</button>
+                <button onClick={(event) => { this.handleClick(event) }}>Click me</button>
 
             </div>
         );
