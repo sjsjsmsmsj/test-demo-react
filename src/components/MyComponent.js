4c62441e9bbe1530d1fc5a67886d6346f4e19f08
sjsjsmsmsj <lamgiathinh05@gmail.com>
2025-06-19 17:54:28 +0700

    add UserInfor component

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,32 +1,14 @@
 import React from 'react'
+import UserInfor from './UserInfor';
 
 class MyComponent extends React.Component {
-    state = {
-        name: 'Thinh', 
-        address: 'Binh dinh', 
-        age: 20
-    }; 
-    
-    handleClick = (event) => {
-        // console.log("Random: ", Math.floor(Math.random() * 100 + 1))
-        this.setState({
-            name: "Lam gia thinh",
-            age: Math.floor(Math.random() * 100 + 1)
-        })
-    }
-
-    handleOnMoreOver(event) {
-        // console.log(event.pageX)
-    }
+  
 
     // JSX
     render() {
         return (
             <div>
-                My name is {this.state.name} and I'm from {this.state.address}
-                <button onMouseOver={this.handleOnMoreOver}>Hover me</button>
-                <button onClick={(event) => { this.handleClick(event) }}>Click me</button>
-
+                <UserInfor/>
             </div>
         );
     }
