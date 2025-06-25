8597167a246938490a3d377d86009801b36f3fa4
sjsjsmsmsj <lamgiathinh05@gmail.com>
2025-06-25 13:54:13 +0700

    video 49

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import { Link } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 import './App.scss';
 import Header from './components/Header/Header';
 
 const App = () => {
   
   return (
-    <div className="App">
-      <Header />
-      <div>
-        test link 
-        <div>
-          <button>
-            <Link to="/users">Go to user page</Link>
-          </button>
-          <button>
-            <Link to="/admins">Go to admin page</Link>
-          </button>
+    <div className="app-container">
+      <div className='header-container'>
+        <Header />
+      </div>
+      <div className='main-container'>
+        <div className='sidenav-container'>
+
+        </div>
+        <div className='app-content'>
+          <Outlet />
         </div>
       </div>
     </div>
--- src/index.js
+++ src/index.js
@@ -17,11 +17,10 @@ root.render(
     {/* <React.StrictMode> */}
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />}></Route>
-        <Route path="users" element={<User />}></Route>
-        <Route path="admins" element={<Admin/>}></Route>
-
-
+        <Route path="/" element={<App />}>
+          <Route path="users" element={<User />}></Route>
+          <Route path="admins" element={<Admin/>}></Route>
+        </Route>
       </Routes>
     </BrowserRouter>
       
