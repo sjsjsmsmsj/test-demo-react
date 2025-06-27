b1296c3b460c125d432578432c73ca78928f6023
sjsjsmsmsj <lamgiathinh05@gmail.com>
2025-06-27 08:02:18 +0700

    video 52

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -19,8 +19,10 @@ const Header = () =>  {
                       <NavLink to='/admins' className='nav-link'>Admin</NavLink>
 
                   </Nav>
-                  <Nav>
-                                  <NavDropdown title="Setting" id="basic-nav-dropdown">
+          <Nav>
+            <button className='btn-login'>Log in</button>
+            <button className='btn-signup'>Sign up</button>
+            {/* <NavDropdown title="Setting" id="basic-nav-dropdown">
               <NavDropdown.Item >Log in</NavDropdown.Item>
               <NavDropdown.Item >
                 Another action
@@ -30,7 +32,7 @@ const Header = () =>  {
               <NavDropdown.Item >
                         Profile
               </NavDropdown.Item>
-            </NavDropdown>
+            </NavDropdown> */}
             </Nav>
         </Navbar.Collapse>
       </Container>
